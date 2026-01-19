b758ae338aa11c632b6df9be328d11e01b24738b
Hassaan Farid <hassaanali994@gmail.com>
2026-01-19 14:27:52 +0500

    Refactor calculator functions and update HTML structure for Saylani assignment

--- app.js
+++ app.js
@@ -1,19 +1,43 @@
-function getNumber(num){
-    var result = document.getElementById('result')
+// function getNumber(num){
+//     var result = document.getElementById('result')
+//     result.value += num;
+// }
+
+// function clearResult(){
+//     var result = document.getElementById('result')
+//     result.value = ''
+// }
+
+// function getresult(){
+//     var result = document.getElementById('result')
+//     result.value = eval(result.value)
+// }
+
+// function backSpace(){
+//     var result = document.getElementById('result')
+//     result.value = result.value.slice(0, -1);
+// }
+
+
+// saylani assignment calculator
+
+
+function inputNumber(num){
+    let result = document.getElementById('input');
     result.value += num;
 }
 
-function clearResult(){
-    var result = document.getElementById('result')
-    result.value = ''
+function clearAll(){
+    let result = document.getElementById("input");
+    result.value = ""
 }
 
-function getresult(){
-    var result = document.getElementById('result')
+function calculate(){
+    let result = document.getElementById("input");
     result.value = eval(result.value)
 }
 
-function backSpace(){
-    var result = document.getElementById('result')
+function backspase(){
+    let result = document.getElementById("input");
     result.value = result.value.slice(0, -1);
 }
