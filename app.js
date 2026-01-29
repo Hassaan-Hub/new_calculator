fabdc2deb088e4421b869414645ea380d0b8afe7
Hassaan Farid <hassaanali994@gmail.com>
2026-01-29 21:49:07 +0500

    udated

--- app.js
+++ app.js
@@ -1,43 +1,52 @@
-// function getNumber(num){
-//     var result = document.getElementById('result')
-//     result.value += num;
-// }
+function getNumber(num){
+    var result = document.getElementById('result')
+    result.value += num;
+}
 
-// function clearResult(){
-//     var result = document.getElementById('result')
-//     result.value = ''
-// }
+function clearResult(){
+    var result = document.getElementById('result')
+    result.value = ''
+}
 
-// function getresult(){
-//     var result = document.getElementById('result')
-//     result.value = eval(result.value)
-// }
+function getresult(){
+    var result = document.getElementById('result')
+    result.value = eval(result.value)
+}
 
-// function backSpace(){
-//     var result = document.getElementById('result')
-//     result.value = result.value.slice(0, -1);
-// }
+function backSpace(){
+    var result = document.getElementById('result')
+    result.value = result.value.slice(0, -1);
+}
+
+
+
+
+
+
+
+
+
 
 
 // saylani assignment calculator
 
 
-function inputNumber(num){
-    let result = document.getElementById('input');
-    result.value += num;
-}
+// function inputNumber(num){
+//     let result = document.getElementById('input');
+//     result.value += num;
+// }
 
-function clearAll(){
-    let result = document.getElementById("input");
-    result.value = ""
-}
+// function clearAll(){
+//     let result = document.getElementById("input");
+//     result.value = ""
+// }
 
-function calculate(){
-    let result = document.getElementById("input");
-    result.value = eval(result.value)
-}
+// function calculate(){
+//     let result = document.getElementById("input");
+//     result.value = eval(result.value)
+// }
 
-function backspase(){
-    let result = document.getElementById("input");
-    result.value = result.value.slice(0, -1);
-}
+// function backspase(){
+//     let result = document.getElementById("input");
+//     result.value = result.value.slice(0, -1);
+// }
